8c68b59456e6ca398a7bd3eedf64cd0bf6151826
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-04-01 12:55:47 +0900

    feat: Breadcrumbs 및 관련 컴포넌트 추가

---
+++ src/components/layout/Breadcrumbs.tsx
@@ -0,0 +1,35 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/Breadcrumb";
+import { Skeleton } from "../ui/Skeleton";
+
+export const Breadcrumbs = ({
+  pageName,
+  isLoading,
+}: {
+  pageName?: string;
+  isLoading?: boolean;
+}) => {
+  return (
+    <Breadcrumb className="h-20 bg-muted rounded-lg border flex items-center justify-start px-6">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbPage className="px-2 py-1 border bg-background rounded-sm">
+          {isLoading ? (
+            <Skeleton className="h-5 w-20" />
+          ) : (
+            <BreadcrumbLink>{pageName || ""}</BreadcrumbLink>
+          )}
+        </BreadcrumbPage>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
