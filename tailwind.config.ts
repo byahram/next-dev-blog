6bbf354cf088c8cba91cede45efd4bee9de16ffb
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-04-10 15:31:22 +0900

    style: code 컬러 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,19 +11,20 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        
+
         accent: "var(--accent)",
         accent_foreground: "var(--accent_foreground)",
-        
+
         muted: "var(--muted)",
         muted_solid: "var(--muted_solid)",
         muted_foreground: "var(--muted_foreground)",
 
         card_bg: "var(--card_bg)",
         card_solid: "var(--card_solid)",
-        
+
         border: "var(--border)",
         input: "var(--input)",
+        code: "var(--code),",
       },
     },
   },
